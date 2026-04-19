ce5432aa8004575f27e62108126847d781f06f1f
Xynergyy <116738372+Xynergyy@users.noreply.github.com>
2026-04-19 23:38:46 +0700

    How static work

--- Assets/Scripts/AnotherObject.cs
+++ Assets/Scripts/AnotherObject.cs
@@ -7,19 +7,21 @@ public class AnotherObject : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-
+            PersistentObject.staticPublicDebugText = "A";
         }
         else if (Input.GetKeyDown(KeyCode.B))
         {
-
+            Debug.Log(PersistentObject.staticPublicDebugText);
         }
         else if (Input.GetKeyDown(KeyCode.C))
         {
-
+            PersistentObject.SetStaticPrivateText("C");
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-
+            var go = GameObject.Find("PersistentObject");
+            var p = go.GetComponent<PersistentObject>();
+            p.SetInstancePrivateText("D");
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
