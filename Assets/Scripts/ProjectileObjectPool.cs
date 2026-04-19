a791353d92e6026d1ea6f9b8c7beddc47163877e
Xynergyy <116738372+Xynergyy@users.noreply.github.com>
2026-04-20 01:58:10 +0700

    Update ProjectileObjectPool.cs

--- Assets/Scripts/ProjectileObjectPool.cs
+++ Assets/Scripts/ProjectileObjectPool.cs
@@ -33,6 +33,7 @@ public class ProjectileObjectPool : MonoBehaviour
             {
                 yield return null;
             }
+        }
 
     }
 
@@ -52,6 +53,7 @@ public class ProjectileObjectPool : MonoBehaviour
 
         var go = projectilePool[0];
         projectilePool.RemoveAt(0);
+        go.SetActive(true);
         return go;
     }
 
